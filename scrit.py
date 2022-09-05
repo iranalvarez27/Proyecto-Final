922c31fbabc5c04f02e0b794b43a4a3631b5c70d
Renato200319 <107636813+Renato200319@users.noreply.github.com>
2022-09-05 18:27:23 -0500

    Update scrit.py

--- scrit.py
+++ scrit.py
@@ -1,1 +1,14 @@
-print("Hellos world")
+
+x =0,
+m =0
+z =0
+char =0
+x =input('Coloca un número: ') 
+char = input('Coloque que operacion quiere realizar(suma = +, resta = -, multiplicación = x, divicion = /): ')
+m = input('Coloca el otro numero: ')
+if char == + :
+    z = x + m
+    print(z)
+elif 
+
+
